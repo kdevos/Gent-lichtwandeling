1bafdebb64f664e76bc6af769f4324927285ac37
Koen De Vos <koendevos1@gmail.com>
2021-02-05 09:02:02 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,239 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01gent-stmichiels1",
+      "name": "01Gent-StMichiels1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.10203463319351691,
+        "pitch": -0.10587054311793054,
+        "fov": 1.3158196073228834
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9632486072463671,
+          "pitch": -0.01273647887017404,
+          "rotation": 6.283185307179586,
+          "target": "1-02gent-blauwe-vogel"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02gent-blauwe-vogel",
+      "name": "02Gent-Blauwe-Vogel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.20675438831318083,
+        "pitch": 0.020816927648152017,
+        "fov": 1.3158196073228834
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6564170154503568,
+          "pitch": 0.03375083667656931,
+          "rotation": 5.497787143782138,
+          "target": "2-03gent-sint-baafs1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03gent-sint-baafs1",
+      "name": "03Gent-Sint-Baafs1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7211571549264715,
+          "pitch": 0.0512566415919018,
+          "rotation": 3.9269908169872414,
+          "target": "5-06gent-vrijdagsmarkt1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04gent-brugje1",
+      "name": "04Gent-Brugje1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.32115221861247534,
+        "pitch": 0.1285570880717728,
+        "fov": 1.3158196073228834
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3193421036619473,
+          "pitch": 0.06871052734840433,
+          "rotation": 0,
+          "target": "4-05gent-kraanlei"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05gent-kraanlei",
+      "name": "05Gent-Kraanlei",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.006712942178408454,
+        "pitch": 0.009452920493735917,
+        "fov": 1.3158196073228834
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7420817808394062,
+          "pitch": 0.07773355469925392,
+          "rotation": 6.283185307179586,
+          "target": "0-01gent-stmichiels1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06gent-vrijdagsmarkt1",
+      "name": "06Gent-Vrijdagsmarkt1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.5021195968830323,
+        "pitch": 0.00566910619961547,
+        "fov": 1.3158196073228834
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2609553489101213,
+          "pitch": 0.008947873347437962,
+          "rotation": 5.497787143782138,
+          "target": "3-04gent-brugje1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Gent tour 1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
